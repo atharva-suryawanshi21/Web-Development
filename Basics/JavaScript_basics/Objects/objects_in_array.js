4559183bf7cca74b6ac0f7a2b0bf7dcf8e890f12
atharva-suryawanshi21 <s.atharva2003@gmail.com>
2024-01-26 01:06:46 +0530

    .

--- Basics/JavaScript_basics/Objects/objects_in_array.js
+++ Basics/JavaScript_basics/Objects/objects_in_array.js
@@ -3,8 +3,16 @@
 const myArray = [
   { name: "Atharva", age: 21 },
   { name: "raiden", age: 31 },
+  { name: "Mokoto", age: 111 },
 ];
-console.log("hi");
-for (const element of myArray) {
+
+for (let element of myArray) {
   console.log(element);
+  console.log(element.name);
 }
+
+// nested desructuring
+const [person1, , { name, age: MokotoAge }] = myArray;
+// 2nd element skipped
+console.log(person1);
+console.log(MokotoAge);
