89f2b4b3ca9520d0587cadf274740dbac5e127f7
atharva-suryawanshi21 <s.atharva2003@gmail.com>
2024-01-25 19:22:14 +0530

    .

--- Basics/JavaScript_basics/Arrays/push_pop.js
+++ Basics/JavaScript_basics/Arrays/push_pop.js
@@ -1,19 +1,23 @@
-'use strict';
+"use strict";
 // We can use both let and const to declare array
 // but using const is preferred because this will give error
-// if we try to redeclare an array, 
+// if we try to redeclare an array,
 // push, pop is allowed but not reassignment of an array
 
-const myArray = ["banana", "apple"]
+const myArray = ["banana", "apple"];
 
 //push and pop elements at end
-myArray.push("grapes")
+myArray.push("grapes");
 console.log(myArray);
-let poppedFruit1 = myArray.pop()
+let poppedFruit1 = myArray.pop();
 console.log("popped item :", poppedFruit1);
 
 // push and pop elements from start
-myArray.unshift("grapes")
+myArray.unshift("grapes");
 console.log(myArray);
-let poppedFruit2 = myArray.shift()
+let poppedFruit2 = myArray.shift();
 console.log("popped item :", poppedFruit2);
+
+// To check if the an object in an array
+console.log(Array.isArray(myArray));
+// typeof myArray -> gives type as object
