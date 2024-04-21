97a6c4d0c9ba6adc0e970a37bf2ca27c8da2e86e
atharva-suryawanshi21 <s.atharva2003@gmail.com>
2024-04-21 09:45:13 +0530

    .

--- Basics/JavaScript_basics/Arrays/clone_concat.js
+++ Basics/JavaScript_basics/Arrays/clone_concat.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
 const array1 = ["item1", "item2"];
 const array2 = ["item3", "item4"];
 
 // clone and concat an array
 // 1
-let newArray = array1.slice(0);
+let newArray = array1.slice(0); // from index 0
 console.log(newArray);
 
 // 2
-newArray = [].concat(array2)
-let concatArray = [].concat(array1, array2)
-console.log(concatArray);
+newArray = [].concat(array2);
+let concatArray = [].concat(array1, array2);
 console.log(newArray);
+console.log(concatArray);
 
 // 3 spread operator
-newArray = [...array1]
+newArray = [...array1];
 console.log(newArray);
